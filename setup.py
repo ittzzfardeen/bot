684c879e8b347f46dc50a18c64be0896ebd167b9
Fardeen khan <ittzzfardeen@gmail.com>
2026-01-19 12:21:21 +0530

    after initialising .egg file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chatbot",
+    version="0.1.0",
+    author="Fardeen_Khan",
+    author_email="ittzzfardeen@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
